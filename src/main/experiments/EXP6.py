5c482f04d03eca08ecb914b599ed82b1036b1b8b
okmtyuta <okmtyuta@gmail.com>
2025-10-20 19:47:34 +0900

    fix

--- src/main/experiments/EXP6.py
+++ src/main/experiments/EXP6.py
@@ -21,7 +21,7 @@ def main() -> None:
         print(f"Slack notification was failed because of {e}")
 
     result = transfer_predict_hl_1(
-        code="EXP2", version="version_0", input_props=[], output_props=["log_halflife"], proteins=proteins
+        code="EXP2", version="version_6", input_props=[], output_props=["log_halflife"], proteins=proteins
     )
     result_dir = Helper.ROOT / "logs" / code
     result_dir.mkdir(parents=True, exist_ok=True)
